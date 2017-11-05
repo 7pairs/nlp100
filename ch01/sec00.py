d3f66bb104339aeb943f4835bcbf2ba1ded856b6
Jun-ya HASEBA <7pairs@gmail.com>
2017-11-05 20:11:17 +0900

    Add answer of 00

---
+++ ch01/sec00.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# http://www.cl.ecei.tohoku.ac.jp/nlp100/#sec00
+
+
+def main():
+    text = 'stressed'
+
+    answer = text[::-1]
+    print(answer)
+
+
+if __name__ == '__main__':
+    main()
