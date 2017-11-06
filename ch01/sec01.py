39ad8f30bd93769e7dfec763d87a008276b00194
Jun-ya HASEBA <7pairs@gmail.com>
2017-11-06 21:22:46 +0900

    Add answer of 01

---
+++ ch01/sec01.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# http://www.cl.ecei.tohoku.ac.jp/nlp100/#sec01
+
+
+def main():
+    text = 'パタトクカシーー'
+
+    answer = text[::2]
+    print(answer)
+
+
+if __name__ == '__main__':
+    main()
